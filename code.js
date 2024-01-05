5b0a34c6f358c2e796f026282308e065a8341008
Anna <jarviscsp@gmail.com>
2024-01-05 17:21:18 +0200

    return json objects array

--- code.js
+++ code.js
@@ -23,12 +23,10 @@ let dataService = {
         this.basketDishes = JSON.parse(localStorage.getItem("basketDishes")) || [];
     }
 }
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.carousel');
     var instances = M.Carousel.init(elems, {
@@ -36,37 +34,61 @@ document.addEventListener('DOMContentLoaded', function() {
         indicators: true
     });
 });
-
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
 });
 
-function LoadData(url)
+// $(document).ready(function(){
+//     alert("jquery alert");
+// });
+
+function LoadData(u_url) 
 {
-    fetch(url)
-    .then(response => response.json())
-    .then(json => 
-    {
-        let ARR = new Array();
-        for (let i = 0; i < json.length; i++) {
-            ARR.push(json[i]);
+    let ARR = new Array();
+    $.ajax({
+        url: u_url,
+        async: false,
+        success: function(json) 
+        {
+            for (let i = 0; i < json.length; i++) 
+            {
+                ARR.push(json[i]);
+                // console.log(json[i]);
+            }
         }
-        console.log(ARR);
-        return ARR;
     });
+    console.log(ARR);
+    return ARR;
 }
 
-
 const JSONPath = "./Pages/JSON/pizzas.json";
-let Pizzas;
-Pizzas = LoadData(JSONPath);
-console.log(Pizzas)
+let Pizzas = LoadData(JSONPath);
+console.log(Pizzas);
 
-let template = document.querySelector("#templateCardPizza").innerHTML;
-let output = document.getElementById("PizzasSection");
-// console.log(output);
-Pizzas.forEach(element => {
-    let html = Mustache.render(template, element);
-    output.insertAdjacentHTML("beforeend", html);
-});
+// function LoadData(url)
+// {
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(json => 
+//     {
+//         let ARR = new Array();
+//         for (let i = 0; i < json.length; i++) 
+//         {
+//             ARR.push(json[i]);
+//             console.log(json[i]);
+//         }
+//         console.log(ARR);
+//         return ARR;
+//     });
+// }
+
+
+
+// let template = document.querySelector("#templateCardPizza").innerHTML;
+// let output = document.getElementById("PizzasSection");
+// // console.log(output);
+// Pizzas.forEach(element => {
+//     let html = Mustache.render(template, element);
+//     output.insertAdjacentHTML("beforeend", html);
+// });
